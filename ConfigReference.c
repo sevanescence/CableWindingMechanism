97bfffcee9d118a47a59d43616e15e55da30db58
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-02 14:20:55 -0500

    Create ConfigReference.c

---
+++ ConfigReference.c
@@ -0,0 +1,5 @@
+#pragma config(Sensor, dgtl1, wireLever, sensorTouch);
+#pragma config(Sensor, dgtl2, resetButton, sensorTouch);
+#pragma config(Sensor, dgtl3, killSwitch, sensorTouch);
+#pragma config(Motor, port1, spoolMotor, tmotorVex269_MC29, openLoop);
+#pragma config(Motor, port2, displayMotor, tmotorVex269_MC29, openLoop);
