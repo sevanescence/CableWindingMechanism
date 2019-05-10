26a84d1c51a6cc85ee7f79b60eba43545bcb70b2
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-10 10:08:41 -0500

    Update progress.c

--- progress.c
+++ progress.c
@@ -4,22 +4,27 @@
 #pragma config(Sensor, dgtl4,  killSwitch,   sensorTouch);
 #pragma config(Motor,  port1,  spoolMotor,   tmotorVex269_MC29, openLoop);
 
-int position = 0;
-int spoolSpeed = 13;
+int position = 0; // current documented position of the motor
+int maxPos = 500; // the maximum distance the spool can unravel
+int spoolSpeed = 13; // the speed at which the spool unravels
 
 bool checkKillSwitch();
-//void reset();
+void reset();
 void turnSpool();
 
 task main() {
+	
+	reset(); // ensures that the spool position is reset before startup.
 
-	while (!checkKillSwitch()) {
+	while (!checkKillSwitch()) { // this will loop as long as the kill switch is not pressed.
 		
-		turnSpool();
+		turnSpool(); // refer to turnSpool() method
 	
 	}
 	
-	untilBump(resetButton);
+	untilBump(resetButton); // reset button must be pressed after kill switch is executed.
+	
+	main(); // reboots program.
 
 }
 
@@ -34,21 +39,47 @@ bool checkKillSwitch() {
 void turnSpool() {
 
 	if (checkKillSwitch() || (SensorValue(turnLever) && SensorValue(backLever))) return;
+	// will not run if either the kill switch is pressed or if both levers are
+	// being pressed at the same time (to avoid bugs).
 	
-	if (SensorValue(turnLever)) {
+	if (SensorValue(resetButton)) { // checks if the reset button is pressed during execution.
+	
+		reset(); // refer to reset() method
+		
+		return;
+	
+	}
+	
+	if (SensorValue(turnLever) && position <= maxPos) {
 	
 		position++;
 		
 		setMotorTarget(spoolMotor, position, spoolSpeed);
 	
-	}
+	} // this will run as long as the position has not reached its maximum.
 	
-	if (SensorValue(backLever)) {
+	if (SensorValue(backLever) && position > 0) {
 	
 		position--;
 		
 		setMotorTarget(spoolMotor, (position * -1), (spoolSpeed) * -1);
 	
-	}
+	} // the spool will never reach below 0.
+
+}
+
+void reset() {
+
+	position = 0;
+	
+	while (SensorValue(spoolMotor) > position) {
+		
+		if (SensorValue(resetButton)) break;
+		
+		setMotorTarget(spoolMotor, SensorValue(spoolMotor) - 1, spoolSpeed);
+		
+	} // the reset mechanism will stop the instant the kill switch is pressed.
+	// although this method of resetting is highly inefficient, the efficiency
+	// was a neccesary sacrifice to ensure safety.
 
 }
