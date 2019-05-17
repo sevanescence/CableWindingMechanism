59b8810df19a5f39fb44ad4cf428dc481912771f
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-17 11:59:39 -0500

    Add files via upload

--- Project.c
+++ Project.c
@@ -5,54 +5,74 @@
 #pragma config(Sensor, dgtl11, motorEncoder, sensorQuadEncoder)
 #pragma config(Motor,  port1,  spoolMotor,   tmotorVex269_MC29, openLoop);
 
-void reset();
+int position = 0;
+
+void reset(); // method used to retract the motor to zero
+void update(); // method used to update the position display
 task main() {
 
-	while (!SensorValue(killSwitch)) {
+	update();
 
-		stopMotor(spoolMotor);
+	while (!SensorValue(killSwitch)) { // will run cable winding program until the kill switch is pressed
 
-		if (SensorValue(resetButton)) {
+		stopMotor(spoolMotor); // stops the motor at the beginning of the while loop to avoid bugs
+
+		if (SensorValue(resetButton)) { // checks if the reset button is pressed before lever operations
 			reset();
-			startTask(main);
-			return;
+			startTask(main); // restarts the program by reinstantiating the main method
+			return; // stops the current main method instance to avoid bugs
 		}
 
 		while (SensorValue(retLever) && SensorValue(motorEncoder) < 0) {
-			if (SensorValue(killSwitch)) break;
+			if (SensorValue(killSwitch)) break; // checks if the kill switch is pressed
 			if (SensorValue(motorEncoder) > -50) {
-				startMotor(spoolMotor, 24);
+				startMotor(spoolMotor, 24); // the motor will slow down if the encoder approaches zero, to avoid bugs
 			} else {
 				startMotor(spoolMotor, 40);
 			}
+			update();
 		}
 
 		while (SensorValue(outLever)) {
-			if (SensorValue(killSwitch)) break;
+			if (SensorValue(killSwitch)) break; // checks if the kill switch is pressed
 			startMotor(spoolMotor, -40);
+			update();
 		}
 
-	} stopMotor(spoolMotor);
+	} stopMotor(spoolMotor); // stops the motor at the end of the loop to avoid bugs in case the kill switch is pressed
+		// if this function were written at the end of the loop block, the motor would lag
 
-	untilBump(resetButton);
+	untilBump(resetButton); // waits for the reset button to be pressed after the kill switch has been pressed
 
-	reset();
+	reset(); // reset method
 
-	startTask(main);
+	startTask(main); // restarts the main program
+
+	// a return is not needed, as the startTask() function is the very end of the main method
 
 }
 
 void reset() {
 
-	while (SensorValue(motorEncoder) < 0) {
-		if (SensorValue(killSwitch)) return;
+	while (SensorValue(motorEncoder) < 0) { // reset operation will run until the spool has completely retracted
+		if (SensorValue(killSwitch)) return; // will instantly stop the operation if the kill switch is pressed
 		if (SensorValue(motorEncoder) < -100) {
 			startMotor(spoolMotor, 40);
 		} else {
 			startMotor(spoolMotor, 24);
 		}
+		update();
 	}
 
 	stopMotor(spoolMotor);
+
+}
+
+// because the startTask() function is written after both reset() methods, it is impossible for the
+// program to bug out if the kill switch is pressed during the reset operation
+
+void update() {
+
+	position = (SensorValue(motorEncoder) / 10) * -1;
 
 }
