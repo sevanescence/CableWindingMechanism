9ec6de7bdb06e19839a5c67c9a85fa4fe8548876
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-17 22:32:31 -0500

    Update Project.c

--- Project.c
+++ Project.c
@@ -5,9 +5,10 @@
 #pragma config(Sensor, dgtl11, motorEncoder, sensorQuadEncoder)
 #pragma config(Motor,  port1,  spoolMotor,   tmotorVex269_MC29, openLoop);
 
-int position = 0;
-string status = "uncontrolled";
-bool resetting = false;
+// global variables used for machine status display
+int position = 0; // position of the motor, recorded by an encoder
+string status = "uncontrolled"; // which lever is currently being pressed to turn the motor
+bool resetting = false; // whether or not the machine is currently executing the reset() operation
 
 void reset(); // method used to retract the motor to zero
 void update(); // method used to update the position display
@@ -17,24 +18,28 @@ task main() {
 
 	while (!SensorValue(killSwitch)) { // will run cable winding program until the kill switch is pressed
 
-		stopMotor(spoolMotor); // stops the motor at the beginning of the while loop to avoid bugs
+		stopMotor(spoolMotor); // stops the motor at the beginning of the while loop to avoid machine lag
 
 		if (SensorValue(resetButton)) { // checks if the reset button is pressed before lever operations
 			reset();
 			startTask(main); // restarts the program by reinstantiating the main method
-			return; // stops the current main method instance to avoid bugs
-		}
+			return; // stops the current main method instance to avoid bugs where the program would
+			        // check for a reset call twice
+		} // the reset block above allows the reset button and kill switch to be mashed as many times as
+		  // the user desires without breaking the program
 
-		while (SensorValue(retLever) && SensorValue(motorEncoder) < 0) {
+		while (SensorValue(retLever) && SensorValue(motorEncoder) < 0) { // retraction lever will only work if the spool is out
 			if (SensorValue(killSwitch)) break; // checks if the kill switch is pressed
 			status = "retracting...";
 			if (SensorValue(motorEncoder) > -50) {
 				startMotor(spoolMotor, 24); // the motor will slow down if the encoder approaches zero, to avoid bugs
+				                            // in which the stopping motor won't go to a complete halt before the
+				                            // encoder reads a value far above zero
 			} else {
-				startMotor(spoolMotor, 40);
+				startMotor(spoolMotor, 40); // as long as the encoder is not approaching zero, the motor will retract
+				                            // consistently
 			}
 			update();
-			status = "uncontrolled";
 		}
 
 		while (SensorValue(outLever)) {
@@ -42,8 +47,9 @@ task main() {
 			status = "releasing...";
 			startMotor(spoolMotor, -40);
 			update();
-			status = "uncontrolled";
 		}
+		
+		status = "uncontrolled";
 
 	} stopMotor(spoolMotor); // stops the motor at the end of the loop to avoid bugs in case the kill switch is pressed
 		// if this function were written at the end of the loop block, the motor would lag
@@ -64,10 +70,10 @@ void reset() {
 
 	while (SensorValue(motorEncoder) < 0) { // reset operation will run until the spool has completely retracted
 		if (SensorValue(killSwitch)) return; // will instantly stop the operation if the kill switch is pressed
-		if (SensorValue(motorEncoder) < -100) {
-			startMotor(spoolMotor, 40);
+		if (SensorValue(motorEncoder) > -100) { // as seen in the retLever loop, this slowing down of the motor
+			startMotor(spoolMotor, 24);     // is to avoid inaccurate record bugs
 		} else {
-			startMotor(spoolMotor, 24);
+			startMotor(spoolMotor, 40);
 		}
 		update();
 	}
@@ -83,6 +89,9 @@ void reset() {
 
 void update() {
 
-	position = (SensorValue(motorEncoder) / 10) * -1;
+	position = (SensorValue(motorEncoder) / 10) * -1; // encoder to measurement formula
+							  // would realistically be more sophisticated and
+							  // involve variables like gear ratio and imperial
+							  // measurement conversions
 
 }
