907d6f064b644b3e5dac056430f658cd392550b1
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-05-17 22:42:04 -0500

    Create MachineLagExample.c

---
+++ MachineLagExample.c
@@ -0,0 +1,28 @@
+#pragma config(Sensor, dgtl1,  retLever,    sensorTouch);
+#pragma config(Sensor, dgtl2,  outLever,    sensorTouch);
+#pragma config(Sensor, dgtl3,  resetButton,  sensorTouch);
+#pragma config(Sensor, dgtl4,  killSwitch,   sensorTouch);
+#pragma config(Sensor, dgtl11, motorEncoder, sensorQuadEncoder)
+#pragma config(Motor,  port1,  spoolMotor,   tmotorVex269_MC29, openLoop);
+
+task main() {
+
+  while (!SensorValue(motorEncoder)) {
+  
+    if (SensorValue(retLever)) {
+    
+      startMotor(spoolMotor, 40);
+    
+    }
+    
+    if (SensorValue(outLever)) {
+    
+      startMotor(spoolMotor, -40);
+    
+    }
+    
+    stopMotor(spoolMotor);
+  
+  } // end of killSwitch while loop
+
+} // end of main task
